727e38c84501a6da7da9506345c1a9c52ebc6841
Zhaohan Weng <zhaohan@rebaselabs.com>
2017-08-16 11:24:01 -0400

    update temp keys

--- lib/twitter_service.rb
+++ lib/twitter_service.rb
@@ -9,8 +9,8 @@ class TwitterService
 
     def client
       Twitter::REST::Client.new do |config|
-        config.consumer_key = ENV['TWITTER_CONSUMER_KEY'] || '5gZ1EGwQuRPGWruU5qbvmRE1L' # temporary token
-        config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET'] || 'UXqZWK1RTaGLxSXrGz1buRGxJgQKrtJWkOiZMa3qL4eysouXCb'
+        config.consumer_key = ENV['TWITTER_CONSUMER_KEY'] || 'Gy3o6gStIo1Y1nHILUvxYQxKv' # temporary token
+        config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET'] || 'W1jlshSoZ4eLnqdOHHFMaAshXMibkCfHjEVWqhxZW65rI3Mz5b'
       end
     end
   end
